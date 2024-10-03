8afa757b926a5854afa4462d580b99e5ff4a55f2
Roman <rabalan83@main.ru>
2024-10-03 13:30:39 +0300

    Добавлен код

---
+++ docs_maker/cli/argparse.py
@@ -0,0 +1,12 @@
+import sys
+import argparse
+
+def cli():
+    parser = argparse.ArgumentParser(prog='doc-maker', description='Конструктор документов')
+    parser.add_argument('--version', '-v', action='version', version=f'%(prog)s Версия 0.1.0')
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
