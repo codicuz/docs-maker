f9c79721908d4b16f888ad6808fc3e4356202f62
Roman <rabalan83@main.ru>
2024-10-03 16:32:42 +0300

    Добавлена БД

--- docs_maker/__init__.py
+++ docs_maker/__init__.py
@@ -1,1 +1,2 @@
 from docs_maker.cli.argparse import cli
+from docs_maker.database.init_db import InitDb
--- docs_maker/cli/argparse.py
+++ docs_maker/cli/argparse.py
@@ -1,12 +1,18 @@
 import sys
 import argparse
+import docs_maker
 
 def cli():
     parser = argparse.ArgumentParser(prog='docs-maker', description='Конструктор документов')
     parser.add_argument('-v', '--version', action='version', version=f'%(prog)s Версия 0.1.0')
+    parser.add_argument('-r', '--run', action='store_true', help='Запустить')
 
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit()
+
+    if args.run:
+        db = docs_maker.InitDb()
+        db.init()
